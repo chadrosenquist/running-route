372db0d3863c37d61cd95f267641189b26b922f3
Chad Rosenquist <ChadRosenquist@hotmail.com>
2017-03-18 20:26:19 -0500

    Added gwt-maps and gwt-log.

--- src/com/kromracing/runningroute/client/GreetingServiceAsync.java
+++ src/com/kromracing/runningroute/client/GreetingServiceAsync.java
@@ -5,7 +5,13 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 /**
  * The async counterpart of <code>GreetingService</code>.
  */
+/* I don't know why the following public interface gives this error?
+ERROR] /D:/Git/running-route/src/com/kromracing/runningroute/client/GreetingServiceAsync.java:[8,8] duplicate class: com.kromracing.runningroute.client.GreetingServiceAsync
+*/
+/*
 public interface GreetingServiceAsync {
+
     void greetServer(String input, AsyncCallback<String> callback)
             throws IllegalArgumentException;
 }
+*/
